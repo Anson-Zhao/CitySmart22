690f703a4c9a145ae1195ba1e9ed600e2b0fc526
zihaowang999 <zihao.wang@g.feitianacademy.org>
2019-05-07 14:52:49 -0400

    Merge branch 'master' of https://github.com/Anson-Zhao/CitySmart22 into Yiyang
    # Conflicts:
    #	.idea/workspace.xml
    #	config/geoCapacity/2019_5_4_15_5_15.xml
    #	config/geoCapacity/2019_5_6_15_6_31.xml
    #	config/geoCapacity/2019_5_7_11_55_47.xml
    #	scripts/Home.js

--- scripts/customPK.js
+++ scripts/customPK.js
@@ -1,28 +1,46 @@
 define(['../src/WorldWind'],function (WorldWind) {
     let customPK = function (color, lat, long) {
-        // wrap up placemark image source
-        let canvas = document.createElement("canvas"),
-            ctx = canvas.getContext('2d'),
-            radius = 10, innerR = 10, outerR = 10, gradient;
 
-        canvas.width = canvas.height = radius * 3;
+         // wrap up World Wind Placemark object
+        let placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+        placemarkAttributes.imageScale = 0.5; //placemark size!
 
         if (Array.isArray(color)) {
-            console.log("Array");
-            innerR = 5;
-            outerR = 12;
-            gradient = ctx.createRadialGradient(radius, radius, innerR, radius, radius, outerR);
+            placemarkAttributes.imageSource = new WorldWind.ImageSource(imagePK(color, 5, 15));
+        } else {
+            placemarkAttributes.imageSource = new WorldWind.ImageSource(imagePK(color, 0, 12));
+        }
+
+        let highlightAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
+        highlightAttributes.imageScale = 2;
+
+        let placemarkPosition = new WorldWind.Position(lat, long, 0);
+
+        this.placemark = new WorldWind.Placemark(placemarkPosition, false, placemarkAttributes);
+        this.placemark.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
+        this.placemark.attributes = placemarkAttributes;
+        this.placemark.highlightAttributes = highlightAttributes;
+    };
+
+    // wrap up placemark image source
+    function imagePK(color, innerR, outerR) {
+        let canvas = document.createElement("canvas"),
+            ctx = canvas.getContext('2d');
+
+        canvas.width = canvas.height = outerR * 2;
+
+        let gradient = ctx.createRadialGradient(outerR, outerR, innerR, outerR, outerR, outerR);
+
+        if (Array.isArray(color)) {
             gradient.addColorStop(0, color[0]);
             gradient.addColorStop(0.5, color[1]);
             gradient.addColorStop(1, color[2]);
         } else {
-            console.log("Not an array");
-            gradient = ctx.createRadialGradient(radius, radius, innerR, radius, radius, outerR);
             gradient.addColorStop(0, color);
         }
 
         ctx.beginPath();
-        ctx.arc(radius, radius, outerR, 0, Math.PI * 2, true);
+        ctx.arc(outerR, outerR, outerR, 0, Math.PI * 2, true);
 
         ctx.fillStyle = gradient;
         ctx.fill();
@@ -31,21 +49,8 @@ define(['../src/WorldWind'],function (WorldWind) {
 
         ctx.closePath();
 
-        // wrap up World Wind Placemark object
-        let placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
-        placemarkAttributes.imageSource = new WorldWind.ImageSource(canvas);
-        placemarkAttributes.imageScale = 0.75; //placemark size!
-
-        let highlightAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
-        highlightAttributes.imageScale = 1.5;
-
-        let placemarkPosition = new WorldWind.Position(lat, long, 0);
-
-        this.placemark = new WorldWind.Placemark(placemarkPosition, true, placemarkAttributes);
-        this.placemark.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
-        this.placemark.attributes = placemarkAttributes;
-        this.placemark.highlightAttributes = highlightAttributes;
-    };
+        return canvas
+    }
 
     return customPK
 });
