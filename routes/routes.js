0d686805949255d6c82ed1cd9b06c3f8c9211ee4
imacbig03 <ansonzmj@gmail.com>
2019-07-16 15:59:11 -0400

    Merge branch 'julia' of https://github.com/Anson-Zhao/CitySmart22 into Carrots
    # Conflicts:
    #	views/signup.ejs

--- routes/routes.js
+++ routes/routes.js
@@ -14,11 +14,13 @@ const rimraf = require("rimraf");
 const mkdirp = require("mkdirp");
 const multiparty = require('multiparty');
 const path    = require('path');
-// const ExpressBrute = require('express-brute');
+const ExpressBrute = require('express-brute');
 const rateLimit = require("express-rate-limit");
 const text = require('textbelt');
 const generator = require('generate-password');
 
+const store = new ExpressBrute.MemoryStore(); // stores state locally, don't use this in production
+const bruteforce = new ExpressBrute(store);
 
 const geoServer = serverConfig.geoServer;
 const Download_From = serverConfig.Download_From;
@@ -296,7 +298,7 @@ module.exports = function (app, passport) {
     });
 
     // process the login form
-    app.post('/login', passport.authenticate('local-login', {
+    app.post('/login', bruteforce.prevent, passport.authenticate('local-login', {
             successRedirect: '/admindetector', // redirect to the secure profile section
             failureRedirect: '/login', // redirect to the login page if there is an error
             failureFlash: true // allow flash messages
@@ -986,7 +988,7 @@ module.exports = function (app, passport) {
         });
     });
 
-    app.post('/userProfile', isLoggedIn, function (req, res) {
+    app.post('/userProfile', bruteforce.prevent, isLoggedIn, function (req, res) {
         res.setHeader("Access-Control-Allow-Origin", "*"); // Allow cross domain header
 
         // new password (User Login)
@@ -1066,7 +1068,7 @@ module.exports = function (app, passport) {
     });
 
     // Update user profile page
-    app.post('/newPass', isLoggedIn, function (req, res) {
+    app.post('/newPass', bruteforce.prevent, isLoggedIn, function (req, res) {
         res.setHeader("Access-Control-Allow-Origin", "*"); // Allow cross domain header
         let user = req.user;
         let newPass = {
@@ -1131,7 +1133,7 @@ module.exports = function (app, passport) {
         });
     });
 
-    app.post('/signup', function (req, res) {
+    app.post('/signup', bruteforce.prevent, function (req, res) {
         res.setHeader("Access-Control-Allow-Origin", "*"); // Allow cross domain header
         // con_CS.query('USE ' + serverConfig.Login_db); // Locate Login DB
 
@@ -1191,7 +1193,7 @@ module.exports = function (app, passport) {
         });
     });
 
-    app.post('/addUser', isLoggedIn, function (req, res) {
+    app.post('/addUser', bruteforce.prevent, isLoggedIn, function (req, res) {
 
         res.setHeader("Access-Control-Allow-Origin", "*"); // Allow cross domain header
         // connection.query('USE ' + serverConfig.Login_db); // Locate Login DB
@@ -1598,7 +1600,7 @@ module.exports = function (app, passport) {
     });
 
     //Submit Request form//
-    app.post('/submitL', function (req, res) {
+    app.post('/submitL', bruteforce.prevent, function (req, res) {
         let result = Object.keys(req.body).map(function (key) {
             return [String(key), req.body[key]];
         });
