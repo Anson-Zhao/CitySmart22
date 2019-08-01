f99d79431401150e7fc81d0e43faeecc67f0b834
ansonzhao <ansonzmj@gmail.com>
2019-08-01 09:33:30 -0400

    Merge branch 'Cucumbers' of https://github.com/Anson-Zhao/CitySmart22 into julia2
    # Conflicts:
    #	.idea/workspace.xml

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -3,7 +3,7 @@ let serverConfig = {
     commondb_connection: {
         'multipleStatements': true,
         'connectionLimit' : 100,
-        'host': '10.11.90.16',
+        'host': 'localhost',
         'user': 'AppUser',
         'password': 'Special888%',
         'port'    :  3306
@@ -11,7 +11,7 @@ let serverConfig = {
     session_connection: {
         'multipleStatements': true,
         'connectionLimit' : 100,
-        'host': '10.11.90.16',
+        'host': 'localhost',
         'user': 'AppUser',
         'password': 'Special888%',
         'port'    :  3306
